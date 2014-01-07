1fe0eca222c6600be87387fc93edb229dd64dba9
Yang Gu <yang.gu@intel.com>
2014-01-07 10:59:19 +0800

    upgrade.py: Use util.py

--- python/upgrade.py
+++ python/upgrade.py
@@ -1,44 +1,11 @@
-#/usr/bin/python
+from util import *
 
-import os
-import argparse
 
-def info(msg):
-    print '[INFO] ' + msg + '.'
-
-def warn(msg):
-    print '[WARNING] ' + msg + '.'
-
-def error(msg):
-    print '[ERROR] ' + msg + '!'
-
-def cmd(msg):
-    print '[COMMAND] ' + msg
-
-def execute(command, ignore_return_value=False):
-    cmd(command)
-    if os.system(command) and not ignore_return_value:
-        error('Failed to execute')
-        quit()
-
-def remove(path):
-    if os.path.exists(path):
-        execute('sudo rm -f ' + path)
-
-def setup_http():
-    execute('echo \'Acquire::http::proxy "http://proxy-shz.intel.com:911";\' >apt.conf')
-    execute('sudo mv apt.conf /etc/apt/')
-    remove('/etc/apt/sources.list.d/google.list')
-
-def setup_socks():
-    remove('/etc/apt/apt.conf')
-    execute('echo \'deb https://dl.google.com/linux/chrome/deb/ stable main\' >google.list')
-    execute('sudo mv google.list /etc/apt/sources.list.d/')
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = 'Script to upgrade system',
-                                     formatter_class = argparse.RawTextHelpFormatter,
-                                     epilog = '''
+def handle_option():
+    global args
+    parser = argparse.ArgumentParser(description='Script to upgrade system',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog='''
 Examples:
   python %(prog)s
   python %(prog)s -t chrome
@@ -47,6 +14,11 @@ Examples:
     parser.add_argument('-t', '--type', dest='type', help='type to upgrade', default='basic', choices=['basic', 'chrome', 'system'])
     args = parser.parse_args()
 
+    if len(sys.argv) <= 1:
+        parser.print_help()
+
+
+def upgrade():
     if args.type == 'basic':
         setup_http()
         execute('sudo apt-get update && sudo apt-get -y dist-upgrade')
@@ -56,3 +28,25 @@ Examples:
     elif args.type == 'system':
         setup_http()
         execute('sudo update-manager -d')
+
+
+def remove(path):
+    if os.path.exists(path):
+        execute('sudo rm -f ' + path)
+
+
+def setup_http():
+    execute('echo \'Acquire::http::proxy "http://proxy-shz.intel.com:911";\' >apt.conf')
+    execute('sudo mv apt.conf /etc/apt/')
+    remove('/etc/apt/sources.list.d/google.list')
+
+
+def setup_socks():
+    remove('/etc/apt/apt.conf')
+    execute('echo \'deb https://dl.google.com/linux/chrome/deb/ stable main\' >google.list')
+    execute('sudo mv google.list /etc/apt/sources.list.d/')
+
+
+if __name__ == "__main__":
+    handle_option()
+    upgrade()
