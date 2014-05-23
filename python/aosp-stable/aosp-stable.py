369227bb633f39c7afd15351080a04ab31e1bb86
Yang Gu <yang.gu@intel.com>
2014-05-23 10:55:25 +0800

    aosp-stable: Remove patch to disable prebuilt webviewchromium

--- python/aosp-stable/aosp-stable.py
+++ python/aosp-stable/aosp-stable.py
@@ -37,7 +37,6 @@ patches_generic_disable_2nd_arch = {
 
 patches_build = {
     'frameworks/webview': ['0001-Change-drawGLFunctor-to-64-bit.patch'],
-    'device/intel/baytrail': ['0001-Disable-prebuilding-webviewchromium.patch'],
     'packages/apps/Browser': [
         '0001-Make-browser-preference-fragment-valid.patch',
         '0002-Fix-a-crash-when-creating-an-incognito-tab-on-tablet.patch',
